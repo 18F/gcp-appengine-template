e3345c2f5ff430ce8c85b76a305763568a22949d
timothy-spencer <timothy.spencer@gsa.gov>
2019-01-24 09:52:51 -0800

    simpler way to do this that doesn't require iam access

--- terraform/enable-kms.sh
+++ terraform/enable-kms.sh
@@ -4,7 +4,7 @@
 # 
 set -e
 
-APPENGINE=$(gcloud iam service-accounts list --format=json | jq -r '.[] | select(.displayName == "App Engine default service account").email')
+APPENGINE=${GOOGLE_PROJECT_ID}@appspot.gserviceaccount.com
 ROLES="roles/cloudkms.admin roles/cloudkms.cryptoKeyEncrypterDecrypter"
 
 for role in $ROLES ; do
