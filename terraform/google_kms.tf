cabeeba2006a0b93640d66d3dc083ab132090bf5
timothy-spencer <timothy.spencer@gsa.gov>
2019-01-24 09:25:01 -0800

    cleaned names up

--- terraform/google_kms.tf
+++ terraform/google_kms.tf
@@ -1,12 +1,12 @@
 resource "google_kms_key_ring" "dataprotectionprovider" {
   project  = "${var.project_id}"
-  name     = "gcp_pilot_key_ring"
+  name     = "dataprotectionprovider"
   location = "${var.region}"
 }
 
 resource "google_kms_crypto_key" "AntiforgeryToken" {
   name     = "Microsoft-AspNetCore-Antiforgery-AntiforgeryToken-v1"
-  key_ring = "${google_kms_key_ring.gcp_pilot_key_ring.id}"
+  key_ring = "${google_kms_key_ring.dataprotectionprovider.id}"
   rotation_period = "100000s"
 
   lifecycle {
