0e9641ef6a90d5bca879485debdee14bbe6f08f5
timothy-spencer <timothy.spencer@gsa.gov>
2019-01-30 18:12:15 -0800

    made separate sso secrets for dev/staging/prod, explicitly set targets, use $() instead of `` so that I can look less like a fossil

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -37,48 +37,49 @@ output "rails_secret_staging" {
   sensitive = true
 }
 
-resource "random_string" "sso_shared_secret" {
+# dev SSO stuff
+resource "random_string" "sso_shared_secret_dev" {
   length  = 32
 }
-output "sso_shared_secret" {
-  value = "${random_string.sso_shared_secret.result}"
-  description = "SSO secret string"
+output "sso_shared_secret_dev" {
+  value = "${random_string.sso_shared_secret_dev.result}"
+  description = "SSO secret string for dev"
   sensitive = true
 }
 
-resource "random_string" "sso_idp_secret" {
+resource "random_string" "sso_idp_secret_dev" {
   length  = 32
 }
-output "sso_idp_secret" {
-  value = "${random_string.sso_idp_secret.result}"
-  description = "SSO idp secret string"
+output "sso_idp_secret_dev" {
+  value = "${random_string.sso_idp_secret_dev.result}"
+  description = "SSO idp secret string for dev"
   sensitive = true
 }
 
-resource "random_string" "sso_cookie_secret" {
+resource "random_string" "sso_cookie_secret_dev" {
   length  = 32
 }
-output "sso_cookie_secret" {
-  value = "${random_string.sso_cookie_secret.result}"
-  description = "SSO cookie secret string"
+output "sso_cookie_secret_dev" {
+  value = "${random_string.sso_cookie_secret_dev.result}"
+  description = "SSO cookie secret string for dev"
   sensitive = true
 }
 
-resource "tls_private_key" "sso" {
+resource "tls_private_key" "sso_dev" {
   algorithm   = "ECDSA"
   ecdsa_curve = "P256"
 }
-output "sso_key" {
-  value = "${tls_private_key.sso.private_key_pem}"
-  description = "SSO JWT key"
+output "sso_key_dev" {
+  value = "${tls_private_key.sso_dev.private_key_pem}"
+  description = "SSO JWT key for dev"
   sensitive = true
 }
-resource "tls_self_signed_cert" "sso" {
+resource "tls_self_signed_cert" "sso_dev" {
   key_algorithm   = "ECDSA"
-  private_key_pem = "${tls_private_key.sso.private_key_pem}"
+  private_key_pem = "${tls_private_key.sso_dev.private_key_pem}"
 
   subject {
-    common_name  = "none"
+    common_name  = "dev"
     organization = "GSA"
   }
 
@@ -90,7 +91,125 @@ resource "tls_self_signed_cert" "sso" {
     "server_auth",
   ]
 }
-output "sso_cert" {
-  value = "${tls_self_signed_cert.sso.cert_pem}"
-  description = "SSO JWT cert"
+output "sso_cert_dev" {
+  value = "${tls_self_signed_cert.sso_dev.cert_pem}"
+  description = "SSO JWT cert for dev"
+}
+
+# staging SSO stuff
+resource "random_string" "sso_shared_secret_staging" {
+  length  = 32
+}
+output "sso_shared_secret_staging" {
+  value = "${random_string.sso_shared_secret_staging.result}"
+  description = "SSO secret string for staging"
+  sensitive = true
+}
+
+resource "random_string" "sso_idp_secret_staging" {
+  length  = 32
+}
+output "sso_idp_secret_staging" {
+  value = "${random_string.sso_idp_secret_staging.result}"
+  description = "SSO idp secret string for staging"
+  sensitive = true
+}
+
+resource "random_string" "sso_cookie_secret_staging" {
+  length  = 32
+}
+output "sso_cookie_secret_staging" {
+  value = "${random_string.sso_cookie_secret_staging.result}"
+  description = "SSO cookie secret string for staging"
+  sensitive = true
+}
+
+resource "tls_private_key" "sso_staging" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P256"
+}
+output "sso_key_staging" {
+  value = "${tls_private_key.sso_staging.private_key_pem}"
+  description = "SSO JWT key for staging"
+  sensitive = true
+}
+resource "tls_self_signed_cert_staging" "sso" {
+  key_algorithm   = "ECDSA"
+  private_key_pem = "${tls_private_key.sso_staging.private_key_pem}"
+
+  subject {
+    common_name  = "staging"
+    organization = "GSA"
+  }
+
+  validity_period_hours = 26280
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
+}
+output "sso_cert_staging" {
+  value = "${tls_self_signed_cert.sso_staging.cert_pem}"
+  description = "SSO JWT cert for staging"
+}
+
+# production SSO stuff
+resource "random_string" "sso_shared_secret_production" {
+  length  = 32
+}
+output "sso_shared_secret_production" {
+  value = "${random_string.sso_shared_secret_production.result}"
+  description = "SSO secret string for production"
+  sensitive = true
+}
+
+resource "random_string" "sso_idp_secret_production" {
+  length  = 32
+}
+output "sso_idp_secret_production" {
+  value = "${random_string.sso_idp_secret_production.result}"
+  description = "SSO idp secret string for production"
+  sensitive = true
+}
+
+resource "random_string" "sso_cookie_secret_production" {
+  length  = 32
+}
+output "sso_cookie_secret_production" {
+  value = "${random_string.sso_cookie_secret_production.result}"
+  description = "SSO cookie secret string for production"
+  sensitive = true
+}
+
+resource "tls_private_key" "sso_production" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P256"
+}
+output "sso_key_production" {
+  value = "${tls_private_key.sso_production.private_key_pem}"
+  description = "SSO JWT key for production"
+  sensitive = true
+}
+resource "tls_self_signed_cert_production" "sso" {
+  key_algorithm   = "ECDSA"
+  private_key_pem = "${tls_private_key.sso_production.private_key_pem}"
+
+  subject {
+    common_name  = "production"
+    organization = "GSA"
+  }
+
+  validity_period_hours = 26280
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
+}
+output "sso_cert_production" {
+  value = "${tls_self_signed_cert.sso_production.cert_pem}"
+  description = "SSO JWT cert for production"
 }
