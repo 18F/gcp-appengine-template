dcc0ee03cd850fde02cace5cf87a340b29078fc6
timothy-spencer <timothy.spencer@gsa.gov>
2019-02-13 15:25:47 -0800

    experimenting

--- terraform/google_firewall.tf
+++ terraform/google_firewall.tf
@@ -3,13 +3,15 @@ resource "google_app_engine_firewall_rule" "flexible_backends" {
   project = "${var.project_id}"
   priority = 1100
   action = "ALLOW"
+  description = "allow access from flexible backends"
   source_range = "10.0.0.1"
 }
 
-# Deny access to everything else
-resource "google_app_engine_firewall_rule" "defaultdeny" {
-  project = "${var.project_id}"
-  priority = 20000
-  action = "DENY"
-  source_range = "*"
-}
+# # Deny access to everything else
+# resource "google_app_engine_firewall_rule" "defaultdeny" {
+#   project = "${var.project_id}"
+#   priority = 20000
+#   action = "DENY"
+#   description = "deny access by default"
+#   source_range = "*"
+# }
