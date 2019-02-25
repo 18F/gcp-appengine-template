725d66cc234d469705d6bf897f1857efe77d0922
timothy-spencer <timothy.spencer@gsa.gov>
2019-02-25 15:18:16 -0800

    some debugging with the proxy signature stuff

--- rails-example/app/controllers/application_controller.rb
+++ rails-example/app/controllers/application_controller.rb
@@ -6,7 +6,12 @@ class ApplicationController < ActionController::Base
 	def check_hmacauth
 		signature_key = ENV.fetch('SIGNATURE_KEY') {''}
 		return if signature_key == ''
-		if request.headers['GAP-Signature'].nil? || ApiAuth.authentic?(request, signature_key, :digest => 'sha1')
+		if request.headers['GAP-Signature'].nil? 
+			pp 'missing GAP-Signature'
+			render plain: "305 use proxy", status: 305
+		end
+		if ApiAuth.authentic?(request, signature_key, :digest => 'sha1')
+			pp 'signature did not verify'
 			render plain: "305 use proxy", status: 305
 		end
 	end
