4a3e1ffadbb64c77d3de252f6307d6acf6a26b51
timothy-spencer <timothy.spencer@gsa.gov>
2019-02-27 16:41:59 -0800

    made ssoproxy an optional add-on, documented things a bit more

--- rails-example/app/controllers/application_controller.rb
+++ rails-example/app/controllers/application_controller.rb
@@ -1,11 +1,55 @@
 class ApplicationController < ActionController::Base
-  before_action :check_hmacauth
+	# check to make sure we are authenticated before going forward
+	before_action :check_auth
 
-  private
+	# set the current user depending on whether we are doing basic auth,
+	# SSO, or local development
+	def current_user
+		idp = ENV.fetch('IDP_PROVIDER_URL') {''}
+		basicauth_username = ENV.fetch('BASICAUTH_USER') {''}
 
-	def check_hmacauth
+		if idp != ''
+			myuser = request.env['HTTP_X_FORWARDED_USER']
+			if myuser.nil?
+				logger.info 'missing HTTP_X_FORWARDED_USER: setting to anonymous'
+				myuser = 'Anonymous'
+			else
+				logger.info "using HTTP_X_FORWARDED_USER from proxy: " + myuser
+			end
+		elsif basicauth_username != ''
+			myuser = basicauth_username
+			logger.info "setting username to BASICAUTH_USER: " + myuser
+		else
+			myuser = 'Anonymous'
+			logger.info "no username set, using " + myuser
+		end
+		@current_user ||= myuser
+	end
+	helper_method :current_user
+
+	# set basic auth if requested, and if we are not using SSO
+	pw = ENV.fetch('BASICAUTH_PASSWORD') {''}
+	idp = ENV.fetch('IDP_PROVIDER_URL') {''}
+	basicauth_username = ENV.fetch('BASICAUTH_USER') {''}
+	if pw != '' && idp == '' then
+		logger.info "setting basic auth password for " + basicauth_username
+		http_basic_authenticate_with name: basicauth_username, password: pw
+	end
+
+	private
+
+	def check_auth
+		# if we don't have a signature key, then
+		# we are running locally and should thus let them in
 		signature_key = ENV.fetch('SIGNATURE_KEY') {''}
 		return if signature_key == ''
+
+		# If we don't have an IDP set, then we are not doing SSO,
+		# and thus should let them in.
+		idp = ENV.fetch('IDP_PROVIDER_URL') {''}
+		return if idp == ''
+
+		# Otherwise, check the HMAC signature
 		if request.headers['GAP-Signature'].nil? 
 			logger.info 'missing GAP-Signature'
 			render plain: "305 use proxy", status: 305
--- rails-example/app/controllers/welcome_controller.rb
+++ rails-example/app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+	if ENV["RAILS_LOG_HEADERS"].present?
+		logger.info self.request.env.select {|k,v| k =~ /^HTTP_/}
+	end
   end
 end
