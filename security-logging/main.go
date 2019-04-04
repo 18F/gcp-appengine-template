0c752992b4a5487410bf9f9962642304a1afa30a
timothy-spencer <timothy.spencer@gsa.gov>
2019-04-04 14:17:49 -0700

    first pass at adding log syncing for GSA IT Security

---
+++ security-logging/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	http.HandleFunc("/", logSyncHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+}
+
+func logSyncHandler(w http.ResponseWriter, r *http.Request) {
+	// Handle healthchecks
+	if r.URL.EscapedPath() == "/liveness_check" {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+		return
+	}
+	if r.URL.EscapedPath() == "/readiness_check" {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+		return
+	}
+
+	// Make sure that this is a request from the cron service.
+	if r.Header.Get("X-Appengine-Cron") != "true" {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Check that we know where logs are
+	from := os.Getenv("FROM")
+	if from == "" {
+		log.Printf("FROM not set in environment")
+		http.Error(w, "do not know where to get the logs", 500)
+		return
+	}
+	to := os.Getenv("TO")
+	if to == "" {
+		log.Printf("TO not set in environment")
+		http.Error(w, "do not know where to put the logs", 500)
+		return
+	}
+
+	// run gsutil rsync to get the logs where they need to go
+	cmd := exec.Command("gsutil", "rsync", "-J", from, to)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Printf("logs failed to sync from %s to %s: %s", from, to, string(output))
+		http.Error(w, "logs failed to sync", 503)
+		return
+	}
+	log.Printf("synced logs from %s to %s", from, to)
+	fmt.Fprint(w, "OK")
+}
--- terraform/google_storage.tf
+++ terraform/google_storage.tf
@@ -1,27 +1,108 @@
-resource "google_storage_bucket" "log-bucket" {
-  name     = "log-bucket-${var.project_id}"
-  location = "${var.region}"
-  storage_class = "REGIONAL"
+# This is the user that is going to be used for syncing logs
+resource "google_service_account" "logviewer" {
+  account_id   = "logviewer"
+  display_name = "Log viewer"
+}
+resource "google_service_account_key" "logviewerkey" {
+  service_account_id = "${google_service_account.logviewer.name}"
+}
+output "logviewer_key" {
+  value = "${base64decode(google_service_account_key.logviewerkey.private_key)}"
+  description = "Private key for logviewer service account"
 }
 
-resource "google_storage_bucket" "storage-bucket" {
-  name     = "storage-bucket-${var.project_id}"
+
+# dev log bucket
+resource "google_storage_bucket" "logs-bucket-dev" {
+  name     = "logs-bucket-dev-${var.project_id}"
   location = "${var.region}"
   storage_class = "REGIONAL"
   versioning = {
     enabled = true
   }
-  logging =  {
-    log_bucket = "${google_storage_bucket.log-bucket.name}"
+  lifecycle_rule = {
+    action = {
+      type = "Delete"
+    }
+    condition = {
+      age = 60
+    }
   }
 }
 
-output "logs_bucket" {
-  value = "${google_storage_bucket.log-bucket.url}"
-  description = "Stores logs from ${google_storage_bucket.storage-bucket.name}"
+resource "google_storage_bucket_acl" "logs-bucket-acl-dev" {
+  bucket = "${google_storage_bucket.logs-bucket-dev.name}"
+
+  role_entity = [
+    "READER:${google_service_account.logviewer.email}",
+  ]
 }
 
-output "pilot_bucket" {
-  value = "${google_storage_bucket.storage-bucket.url}"
-  description = "Example Google Storage bucket"
+output "logs_bucket_dev" {
+  value = "${google_storage_bucket.logs-bucket-dev.url}"
+  description = "Logs bucket for dev logs that are exported for ingestion by GSA IT Security"
+}
+
+
+# staging log bucket
+resource "google_storage_bucket" "logs-bucket-staging" {
+  name     = "logs-bucket-staging-${var.project_id}"
+  location = "${var.region}"
+  storage_class = "REGIONAL"
+  versioning = {
+    enabled = true
+  }
+  lifecycle_rule = {
+    action = {
+      type = "Delete"
+    }
+    condition = {
+      age = 60
+    }
+  }
+}
+
+resource "google_storage_bucket_acl" "logs-bucket-acl-staging" {
+  bucket = "${google_storage_bucket.logs-bucket-staging.name}"
+
+  role_entity = [
+    "READER:${google_service_account.logviewer.email}",
+  ]
+}
+
+output "logs_bucket_staging" {
+  value = "${google_storage_bucket.logs-bucket-staging.url}"
+  description = "Logs bucket for staging logs that are exported for ingestion by GSA IT Security"
+}
+
+
+# prod log bucket
+resource "google_storage_bucket" "logs-bucket-prod" {
+  name     = "logs-bucket-prod-${var.project_id}"
+  location = "${var.region}"
+  storage_class = "REGIONAL"
+  versioning = {
+    enabled = true
+  }
+  lifecycle_rule = {
+    action = {
+      type = "Delete"
+    }
+    condition = {
+      age = 60
+    }
+  }
+}
+
+resource "google_storage_bucket_acl" "logs-bucket-acl-prod" {
+  bucket = "${google_storage_bucket.logs-bucket-prod.name}"
+
+  role_entity = [
+    "READER:${google_service_account.logviewer.email}",
+  ]
+}
+
+output "logs_bucket_master" {
+  value = "${google_storage_bucket.logs-bucket-prod.url}"
+  description = "Logs bucket for production logs that are exported for ingestion by GSA IT Security"
 }
