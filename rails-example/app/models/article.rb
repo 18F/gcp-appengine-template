67b33efe966d1501572ae0ce35078825c20c244a
timothy-spencer <timothy.spencer@gsa.gov>
2019-04-23 14:03:47 -0700

    added rails-example comments to link controls with code

--- rails-example/app/models/article.rb
+++ rails-example/app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
-  validates :title, presence: true,
-                    length: { minimum: 5 }
+	# NIST-800-53-SI-10
+	validates :title, presence: true,
+				length: { minimum: 5 }
 end
