af3a8056b0cadcab795ab354a0fa3ed359df0ec5
timothy-spencer <timothy.spencer@gsa.gov>
2019-04-25 17:04:45 -0700

    fix up audit log script to be smarterer

--- gcp_setup/enable-audit-logs.sh
+++ gcp_setup/enable-audit-logs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+#
+# Set up audit logs on storage and kms accesses
+#
 set -e
 
-gcloud projects get-iam-policy $GOOGLE_PROJECT_ID > $GOOGLE_PROJECT_ID.iam.policy.yml
+gcloud projects get-iam-policy "$GOOGLE_PROJECT_ID" > "$GOOGLE_PROJECT_ID.iam.policy.yml"
 
-cat <<EOF >> $GOOGLE_PROJECT_ID.iam.policy.yml
+cat <<EOF > "$GOOGLE_PROJECT_ID.iam.policy.yml.new"
 auditConfigs:
 - auditLogConfigs:
   - logType: DATA_WRITE
@@ -15,4 +18,16 @@ auditConfigs:
   service: cloudkms.googleapis.com
 EOF
 
-gcloud projects set-iam-policy $GOOGLE_PROJECT_ID $GOOGLE_PROJECT_ID.iam.policy.yml
+# find common lines between our new policy and the one already installed
+comm -12 "$GOOGLE_PROJECT_ID.iam.policy.yml" "$GOOGLE_PROJECT_ID.iam.policy.yml.new" > "$GOOGLE_PROJECT_ID.iam.policy.yml.common"
+
+# if the common lines and the new policy addition are the same, it's already up there
+if cmp "$GOOGLE_PROJECT_ID.iam.policy.yml.new"  "$GOOGLE_PROJECT_ID.iam.policy.yml.common" >/dev/null ; then
+	echo auditing policy is already set
+else
+	cat "$GOOGLE_PROJECT_ID.iam.policy.yml.new" >> "$GOOGLE_PROJECT_ID.iam.policy.yml"
+	gcloud projects set-iam-policy "$GOOGLE_PROJECT_ID" "$GOOGLE_PROJECT_ID.iam.policy.yml"
+fi
+
+# clean up
+rm -f "$GOOGLE_PROJECT_ID.iam.policy.yml" "$GOOGLE_PROJECT_ID.iam.policy.yml.new" "$GOOGLE_PROJECT_ID.iam.policy.yml.common"
