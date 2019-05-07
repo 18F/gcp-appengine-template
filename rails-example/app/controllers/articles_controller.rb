9f6ea3eb94859b6bf8183724409ad1079fdf547b
timothy-spencer <timothy.spencer@gsa.gov>
2019-05-07 14:50:58 -0700

    redirects seem to not be going properly

--- rails-example/app/controllers/articles_controller.rb
+++ rails-example/app/controllers/articles_controller.rb
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
  
     if @article.update(article_params)
-      redirect_to @article
+      redirect_to articles_path
     else
       render 'edit'
     end
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
    
     if @article.save
-      redirect_to @article
+      redirect_to articles_path
     else
       render 'new'
     end
