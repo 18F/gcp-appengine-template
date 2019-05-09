8c75223fa01c71231d4bdd13eab78686f5c31626
timothy-spencer <timothy.spencer@gsa.gov>
2019-05-09 10:09:21 -0700

    trying x-forwarded-host header

--- rails-example/app/controllers/articles_controller.rb
+++ rails-example/app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
  
-    redirect_to "/articles"
+    redirect_to articles_path
   end
 
   def index
